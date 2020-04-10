21eac08ba833b72da27d9023e641cca6d2f8980a
Alex Martinez <alexmartinez0071234@gmail.com>
2020-04-10 11:48:17 -0400

    project post and get is done

--- actionsRouter.js
+++ actionsRouter.js
@@ -1,5 +1,15 @@
 const express = require("express");
-const Actions= require("./data/helpers/actionModel");
+const Actions = require("./data/helpers/actionModel");
 const router = express.Router();
+
+router.get("/", (req, res) => {
+  Actions.get(req.query)
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch((err) => {
+      console.log("messed up getting the array of projects", err);
+    });
+});
 
 module.exports = router;
--- projectsRouter.js
+++ projectsRouter.js
@@ -2,4 +2,37 @@ const express = require("express");
 const Projects = require("./data/helpers/projectModel");
 const router = express.Router();
 
+router.get("/:id", (req, res) => {
+  Projects.get(req.params.id)
+    .then((project) => {
+      if (project) {
+        res.status(200).json(project);
+      } else {
+        res.status(404).json({ message: "project can not be found" });
+      }
+    })
+    .catch((err) => {
+      console.log("messed up getting the projects", err);
+    });
+});
+
+router.post("/", (req, res) => {
+  const newProject = req.body;
+  const { name, description } = req.body;
+  Projects.insert(newProject)
+    .then((project) => {
+      if (project) {
+        res.status(201).json(project);
+      } else if (!name || !description) {
+        res
+          .status(400)
+          .json({ message: "name or description is needed to make a project" });
+      } else {
+        res.status(500).json({ error: "error trying to add a new project" });
+      }
+    })
+    .catch((err) => {
+      console.log("messed up adding a new project", err);
+    });
+});
 module.exports = router;
